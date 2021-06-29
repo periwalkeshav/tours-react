7bc3e0a8027797c21a584a75b81c87d66cdb3c76
periwalkeshav <keshav.periwal2018@vitstudent.ac.in>
2021-06-29 21:35:23 +0530

    Tour React Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+import {data} from './data'
 function App() {
+  const [isLoading, setisLoading] = useState(true)
+  const [tours, setTour] = useState([])
+  const removeTour = (id)=>{
+    let newTours = tours.filter((tour)=> tour.id!==id)
+    setTour( newTours)
+  }
+  const fetchTours = ()=>{
+    setisLoading(true)
+    try {
+      // const res = await fetch(url)
+      // const tours = await res.json()
+      setisLoading(false)
+      setTour(data)
+    } catch (error) {
+      setisLoading(false)
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    fetchTours()
+  },[])
+if (isLoading) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Loading />
+    </main>
+  )
+}
+if(tours.length===0){
+  return (
+    <main>
+      <div className="title">
+        <h2>No Tours Left</h2>
+        <button className="btn" onClick={()=> fetchTours()}>Refresh</button>
+      </div>
+    </main>
+  )
+}
+  return (<main>
+    <Tours tours={tours} removeTour={removeTour}></Tours>
+  </main>)
 }
 
-export default App;
+export default App
